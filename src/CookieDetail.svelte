d3f483a5cf4cf9ee548acee517ff149e058222ea
anna boone <anna.boone@startribune.com>
2019-11-26 09:01:53 -0600

    fixing print css

--- src/CookieDetail.svelte
+++ src/CookieDetail.svelte
@@ -2,13 +2,37 @@
   export let recipe;
 </script>
 
+<div class="print">
+  <h1>{recipe.name}</h1>
+  <p class="byline">{recipe.year} recipe from <b>{recipe.baker[0].name}</b> of {recipe.baker[0].hometown}.</p>
+  {#if recipe.notes}
+    <h2>Note</h2>
+    <p class="note">{recipe.notes}</p>
+    {#if recipe.notes}
+      <h2>Note</h2>
+      <p class="note">{recipe.notes}</p>
+    {/if}
+
+    <p class="yield">{recipe.recipe_yield}</p>
+
+    <h2>Ingredients</h2>
+
+    {@html recipe.ingredients_clean}
+
+
+    <h2>Directions</h2>
+
+    {@html recipe.procedure_clean}
+  {/if}
+</div>
+
 {#if recipe}
 <div class="recipe-detail">
   <div class="headline">
     <h1>{recipe.name}</h1>
   </div>
   <h3 class="blurb">{recipe.summary}</h3>
-  <div class="image" style="background: url({recipe.thumbnail}); background-size: cover; background-position: 50% 80%; background-repeat: no-repeat;"></div>
+  <div class="image" style="background: url({recipe.thumbnail}) 50% 80% / cover no-repeat;"></div>
   <!-- <img alt="{recipe.name}" class="cookie-thumbnail" src="{recipe.thumbnail}"/> -->
   <div class="text">
     <p class="byline">{recipe.year} recipe from <b>{recipe.baker[0].name}</b> of {recipe.baker[0].hometown}.</p>
